2f32ff512992b7aca977b2c227c9449b83d99e57
Deaf-Web-Admin <linux.user@techie.com>
2023-08-19 16:26:20 +0200

    fuaaa, me costo la logica de las operaciones, mas que todo la de suma, y usar mas de un digito, pero, ahitaaaaa, PROYECTO CONCLUIDO

--- js.js
+++ js.js
@@ -1,31 +1,58 @@
-//Recuperamos el input y el boton
-const BotonConNumero = document.querySelectorAll(".Numero");
-const BotonLimpiar = document.querySelector(".BotonC");
 const Pantalla = document.querySelector(".LCDScreen");
 
-const cuandoSeHaceClick = function (evento) {
+const BotonConNumero = document.querySelectorAll(".Numero");
+const cuandoSeHaceClick = function () {
 	Pantalla.value = Pantalla.value + this.innerText;
+	localStorage.setItem("NumeroGuardado", Pantalla.value);
 };
-
-const LimpiarTodo = function (evento) {
-	Pantalla.value = "";
-};
-
 BotonConNumero.forEach((boton) => {
 	boton.addEventListener("click", cuandoSeHaceClick);
 });
 
+const BotonConFuncion = document.querySelectorAll(".Funcion");
+const AlmacenarFuncion = function () {
+	localStorage.setItem("Operacion", this.innerText);
+	localStorage.setItem(
+		"NumeroGuardado2",
+		localStorage.getItem("NumeroGuardado")
+	);
+	Pantalla.value = Pantalla.value + this.innerText;
+	localStorage.setItem("NumeroGuardado", "");
+	Pantalla.value = "";
+};
+BotonConFuncion.forEach((boton) => {
+	boton.addEventListener("click", AlmacenarFuncion);
+	localStorage.setItem("NumeroGuardado", "");
+});
+
+const BotonLimpiar = document.querySelector(".BotonC");
+const LimpiarTodo = function () {
+	Pantalla.value = "";
+	localStorage.setItem("Operacion", "");
+	localStorage.setItem("NumeroGuardado", "");
+	localStorage.setItem("NumeroGuardado2", "");
+};
 BotonLimpiar.addEventListener("click", LimpiarTodo);
 
-function Sumar(num1, num2) {
-	return num1 + num2;
-}
-function Restar(num1, num2) {
-	return num1 - num2;
-}
-function Multiplicar(num1, num2) {
-	return num1 * num2;
-}
-function Dividir(num1, num2) {
-	return num1 / num2;
-}
+const BotonDeResultado = document.querySelector(".Total");
+const ImprimirResultado = function () {
+	let ElPrimero = localStorage.getItem("NumeroGuardado2");
+	let ElSegundo = localStorage.getItem("NumeroGuardado");
+	if (localStorage.getItem("Operacion") == "+") {
+		Pantalla.value = Number(ElPrimero) + Number(ElSegundo);
+		setTimeout(LimpiarTodo, 5000);
+	} else if (localStorage.getItem("Operacion") == "-") {
+		Pantalla.value = ElPrimero - ElSegundo;
+		setTimeout(LimpiarTodo, 5000);
+	} else if (localStorage.getItem("Operacion") == "*") {
+		Pantalla.value = ElPrimero * ElSegundo;
+		setTimeout(LimpiarTodo, 5000);
+	} else if (localStorage.getItem("Operacion") == "/") {
+		Pantalla.value = ElPrimero / ElSegundo;
+		setTimeout(LimpiarTodo, 5000);
+	} else {
+	}
+};
+BotonDeResultado.addEventListener("click", ImprimirResultado);
+
+console.log("Gracias a mis compis por el reto, ayuda a no oxidarme en JS :)");
